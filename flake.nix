ca1951fdb2dfd0a43d1d0da7b852453bfffe63e6
Prunkles <prunkles.dreemurr@gmail.com>
2023-12-21 03:34:49 +0200

    chore: Add basic Nix flake

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Placies";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nuget-packageslock2nix = {
+      url = "github:mdarocha/nuget-packageslock2nix/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ self, flake-parts, nuget-packageslock2nix, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      perSystem = { system, pkgs, ... }:
+        let
+          dotnet-sdk = pkgs.dotnet-sdk_8;
+          dotnet-aspnetcore-runtime = pkgs.dotnet-aspnetcore_8;
+        in {
+          packages.placies-allowlist-proxy-gateway = pkgs.buildDotnetModule {
+            pname = "Placies.Gateways.AllowlistProxyGateway.Server";
+            version = "0.0.1";
+            src = self;
+            projectFile = "src/Gateways/Placies.Gateways.AllowlistProxyGateway.Server/Placies.Gateways.AllowlistProxyGateway.Server.fsproj";
+            inherit dotnet-sdk;
+            dotnet-runtime = dotnet-aspnetcore-runtime;
+            nugetDeps = nuget-packageslock2nix.lib {
+              inherit system;
+              lockfiles = [ ./src/Gateways/Placies.Gateways.AllowlistProxyGateway.Server/packages.lock.json ];
+            };
+          };
+        };
+    };
+}
