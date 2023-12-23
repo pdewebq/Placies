702511533c6633e1eb793d9c349af430b8d13e43
Prunkles <prunkles.dreemurr@gmail.com>
2023-12-24 00:35:46 +0200

    chore(nix): Add docker image package

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
         let
           dotnet-sdk = pkgs.dotnet-sdk_8;
           dotnet-aspnetcore-runtime = pkgs.dotnet-aspnetcore_8;
-        in {
+        in rec {
           packages.placies-allowlist-proxy-gateway = pkgs.buildDotnetModule {
             pname = "Placies.Gateways.AllowlistProxyGateway.Server";
             version = "0.0.1";
@@ -27,6 +27,20 @@
             nugetDeps = nuget-packageslock2nix.lib {
               inherit system;
               lockfiles = [ ./src/Gateways/Placies.Gateways.AllowlistProxyGateway.Server/packages.lock.json ];
+            };
+          };
+          packages.placies-allowlist-proxy-gateway-docker-image = pkgs.dockerTools.buildLayeredImage {
+            name = "placies-allowlist-proxy-gateway-docker-image";
+            contents = [
+              pkgs.busybox
+              pkgs.cacert
+              packages.placies-allowlist-proxy-gateway
+            ];
+            config = {
+              Env = [
+                "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+              ];
+              Cmd = [ "/bin/Placies.Gateways.AllowlistProxyGateway.Server" ];
             };
           };
         };
